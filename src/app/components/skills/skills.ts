93a049795688a9cebdb35f16cb6fe519bbd51b05
eXactDevFlaw <lutzboelling@gmx.de>
2026-05-13 23:05:10 +0200

    feat: added new assets as svg

--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -13,16 +13,17 @@ interface Skill {
 })
 export class SkillsComponent {
   readonly skills: Skill[] = [
-    { name: 'Angular',         img: 'img/skill-angular.png' },
-    { name: 'TypeScript',      img: 'img/skill-typescript.png' },
-    { name: 'JavaScript',      img: 'img/skill-javascript.png' },
-    { name: 'HTML',            img: 'img/skill-html.png' },
-    { name: 'CSS',             img: 'img/skill-css.png' },
-    { name: 'REST-API',        img: 'img/skill-rest-api.png' },
-    { name: 'Supabase',        img: 'img/skill-supabase.png' },
-    { name: 'Git',             img: 'img/skill-git.png' },
-    { name: 'Material Design', img: 'img/skill-material-design.png' },
-    { name: 'Scrum',           img: 'img/skill-scrum.png' },
+    { name: 'Angular',         img: 'img/skill-angular.svg' },
+    { name: 'TypeScript',      img: 'img/skill-typescript.svg' },
+    { name: 'JavaScript',      img: 'img/skill-javascript.svg' },
+    { name: 'HTML',            img: 'img/skill-html.svg' },
+    { name: 'CSS',             img: 'img/skill-css.svg' },
+    { name: 'REST-API',        img: 'img/skill-rest-api.svg' },
+    { name: 'Supabase',        img: 'img/skill-supabase.svg' },
+    { name: 'Firebase',        img: 'img/skill-firebase.svg' },
+    { name: 'Git',             img: 'img/skill-git.svg' },
+    { name: 'Material Design', img: 'img/skill-material-design.svg' },
+    { name: 'Scrum',           img: 'img/skill-scrum.svg' },
   ];
 
   scrollToContact() {
